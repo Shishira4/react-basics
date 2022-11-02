369caf5673628e2af3e05de214239584247a2be9
Shishira4 <shivashishira@gmail.com>
2022-11-02 11:36:17 +0530

    hooks

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 const Home = () => {
-    let name = 'mario';
+    //let name = 'mario';//not reactive, to make it reactive use a hook 
+    const [name, setName]=useState('mario');
+    const [age, setAge] =useState(10);
     const handleClick = (e) =>{
-        
+        setName('luigi');
+        setAge(19);
     }
     
     
@@ -9,7 +13,7 @@ const Home = () => {
         <div className="Home">
             <h2>Home Page</h2>
             <button onClick={handleClick}>Click me!</button>
-            
+            <p>{name} is {age} years old</p>
         </div>
      );
 }
