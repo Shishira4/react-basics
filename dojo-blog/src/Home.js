1adc9754f79f790feee5a76143e02aa21730f05b
Shishira4 <shivashishira@gmail.com>
2022-11-02 11:13:25 +0530

    basics

---
+++ dojo-blog/src/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+    let name = 'mario';
+    const handleClick = (e) =>{
+        
+    }
+    
+    
+    return ( 
+        <div className="Home">
+            <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me!</button>
+            
+        </div>
+     );
+}
+ 
+export default Home;
