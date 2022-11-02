3659e860592d4ea3dcffc1b9f1adfc06ae29b130
Shishira4 <shivashishira@gmail.com>
2022-11-02 13:15:03 +0530

    Lists

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
 const Home = () => {
-    //let name = 'mario';//not reactive, to make it reactive use a hook 
-    const [name, setName]=useState('mario');
-    const [age, setAge] =useState(10);
-    const handleClick = (e) =>{
-        setName('luigi');
-        setAge(19);
-    }
-    
-    
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
     return ( 
         <div className="Home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Click me!</button>
-            <p>{name} is {age} years old</p>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+            
         </div>
      );
 }
